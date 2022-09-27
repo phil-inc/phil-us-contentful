2b87187b96b41c6cdb56852f31fe46b4fe5a0098
shailesz <shailesz@duck.com>
2022-09-27 15:36:39 +0545

    redesign patients page

--- src/pages/patients.tsx
+++ src/pages/patients.tsx
@@ -3,6 +3,7 @@ import {Layout} from 'layouts/Layout/Layout';
 
 import {
 	ActionIcon,
+	Box,
 	Button,
 	Card,
 	Center,
@@ -31,6 +32,7 @@ import {IconSearch, IconArrowRight, IconArrowLeft} from '@tabler/icons';
 import {FAQ} from 'components/common/FAQ';
 import {useStaticQuery, graphql} from 'gatsby';
 import type {PatientPageType} from 'types/patientsPage';
+import {InfoBox} from 'components/common/InfoBox';
 
 const useStyles = createStyles(theme => ({
 	inner: {
@@ -62,6 +64,13 @@ const useStyles = createStyles(theme => ({
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
+	infoBox: {
+		transform: 'translate(0.5%, -250px)',
+
+		[`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+			transform: 'translate(0, 0)',
+		},
+	},
 }));
 
 export default function Patients({data}) {
@@ -70,12 +79,10 @@ export default function Patients({data}) {
 	const {firstTwoColumnSection, benefitSection, testimonialsSection, faqSection}
 		= data.contentfulPatientPage as PatientPageType;
 
-	console.log('data', faqSection);
-
 	return (
 		<Layout>
 			{/* Hero Section */}
-			<Grid gutter={'xs'} align='center' mb={160}>
+			<Grid gutter={'xs'} align='center' mb={100}>
 				<Grid.Col lg={6} md={6} sm={12}>
 					<Title order={2} mb={16}>
 						{firstTwoColumnSection.title[0]}
@@ -98,73 +105,16 @@ export default function Patients({data}) {
 							></StaticImage>
 						</Container>
 					</Container>
+					<Box className={classes.infoBox}>
+						<InfoBox title='Don’t Have a Phil Login?' noButton color='yellow'>
+							*If your doctor sent your prescription to Phil, we’ll text you to confirm your order within one
+							business day. If you haven’t received a message from us, please complete this [form].
+						</InfoBox>
+					</Box>
 				</Grid.Col>
 			</Grid>
 
 			{/* Second Section */}
-			<Expanded>
-				<Center>
-					<Title mb={64} order={2}>
-						Benefits
-					</Title>
-				</Center>
-				<Grid mb={52}>
-					{benefitSection.map(item => (
-						<Grid.Col span={6}>
-							<Benefit divider={item.divider} title={item.title}>
-								{item.description.description}
-							</Benefit>
-						</Grid.Col>
-					))}
-				</Grid>
-			</Expanded>
-
-			{/* Third Section */}
-			<Grid gutter={60} align='end' mb={160}>
-				<Grid.Col lg={6} md={6} sm={12}>
-					<Container style={{background: '#f4f4f4'}}>
-						<Center>
-							<StaticImage
-								src='../assets/images/index_hero.png'
-								alt='circles inside circles'
-								className='hero-img'
-								placeholder='blurred'
-								layout='constrained'
-							></StaticImage>
-						</Center>
-					</Container>
-				</Grid.Col>
-				<Grid.Col lg={6} md={6} sm={12}>
-					<Container p={0} mb={32}>
-						<Title order={2} style={{maxWidth: 601}} mb={32}>
-							Fill a prescription
-						</Title>
-					</Container>
-					<Container p={0} mb={150}>
-						<Title order={3} style={{maxWidth: 601}}>
-							Login to your Phil account
-						</Title>
-						<Divider variant='dashed' size='lg' mb={25} />
-						<Button color='dark'>Patient log in</Button>
-					</Container>
-
-					<Container p={0}>
-						<Text size='lg' weight='bold'>
-							Don’t Have a Phil Login?
-						</Text>
-						<Text size='lg' mb={25}>
-							*If your doctor sent your prescription to Phil, we’ll text you to confirm your order within one
-							business day. If you haven’t received a message from us, please{' '}
-							<Text span italic size={'lg'} style={{color: '#5ABEA4'}}>
-								contact us
-							</Text>
-							.
-						</Text>
-					</Container>
-				</Grid.Col>
-			</Grid>
-
-			{/* Fourth Section */}
 			<Expanded background='#00827E' minHeight={813}>
 				<Center mb={62}>
 					<Title order={2} mt={12} color='white'>
@@ -187,7 +137,7 @@ export default function Patients({data}) {
 				</Grid>
 			</Expanded>
 
-			{/* Fifth Section */}
+			{/* Third Section */}
 			<Container fluid>
 				<Container fluid>
 					<Center>
