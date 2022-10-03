66125e906bac52d87aa22362ff0ecb7e53f05f11
shailesz <shailesz@duck.com>
2022-10-03 09:53:50 +0545

    Create Page Templates

---
+++ gatsby-node.js
@@ -0,0 +1,80 @@
+const slugify = require('slugify')
+
+exports.createPages = async function ({ actions, graphql }) {
+    const { data } = await graphql(`
+    query getPages {
+        allContentfulPage {
+          nodes {
+           id
+              title
+              sections {
+                  ... on ContentfulReferencedSection {
+                      id
+                      header
+                      sectionType
+                      references {
+                          linkTo
+                          heading
+                          buttonText
+                          asset {
+                              gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED, resizingBehavior: SCALE)
+                              id
+                          }
+                          body {
+                              raw
+                          }
+                          author
+                          designation
+                      }
+                      referenceType
+                      linkTo
+                      buttonText
+                  }
+                  ... on ContentfulSection {
+                      id
+                      body {
+                          raw
+                          references {
+                              contentful_id
+                              __typename
+                              description
+                              gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+                          }
+                      }
+                      asset {
+                          gatsbyImageData(resizingBehavior: SCALE, placeholder: BLURRED, layout: CONSTRAINED)
+                          title
+                      }
+                      buttonText
+                      header
+                      sectionType
+                      linkTo
+                      sys {
+                          contentType {
+                              sys {
+                                  id
+                              }
+                          }
+                      }
+                      subHeader {
+                          subHeader
+                      }
+                  }
+              }
+          }
+        }
+      }
+      
+    `)
+
+    data.allContentfulPage.nodes.forEach(({title}) => {
+
+      const slug = slugify(title, {lower: true})
+
+      actions.createPage({
+        path: title === 'Home' ? '/' : slug,
+        component: require.resolve(`./src/templates/page.tsx`),
+        context: { title: title },
+      })
+    })
+  }
---
+++ src/templates/page.tsx
@@ -0,0 +1,94 @@
+import React from 'react';
+import {Layout} from 'layouts/Layout/Layout';
+import type {ContentfulPage} from 'types/page';
+import Section from 'components/section/Section';
+import {graphql} from 'gatsby';
+
+type PageTemplateProps = {
+	data: {contentfulPage: ContentfulPage};
+};
+
+const PageTemplate: React.FC<PageTemplateProps> = ({data}) => {
+	const {id, sections, title} = data.contentfulPage;
+
+	console.log({id, sections, title});
+
+	let basicSectionCount = 0;
+
+	return (
+		<Layout>
+			{sections.map((section, index) => {
+				if (section.sectionType === 'Basic Section') {
+					return <Section key={section.id} section={section} index={basicSectionCount++} />;
+				}
+
+				return <Section key={section.id} section={section} />;
+			})}
+		</Layout>
+	);
+};
+
+export const pageQuery = graphql`
+	query getPage($title: String) {
+		contentfulPage(title: {eq: $title}) {
+			id
+			title
+			sections {
+				... on ContentfulReferencedSection {
+					id
+					header
+					sectionType
+					references {
+						linkTo
+						heading
+						buttonText
+						asset {
+							gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED, resizingBehavior: SCALE)
+							id
+						}
+						body {
+							raw
+						}
+						author
+						designation
+					}
+					referenceType
+					linkTo
+					buttonText
+				}
+				... on ContentfulSection {
+					id
+					body {
+						raw
+						references {
+							contentful_id
+							__typename
+							description
+							gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+						}
+					}
+					asset {
+						gatsbyImageData(resizingBehavior: SCALE, placeholder: BLURRED, layout: CONSTRAINED)
+						title
+					}
+					buttonText
+					header
+					sectionType
+					linkTo
+					sys {
+						contentType {
+							sys {
+								id
+							}
+						}
+					}
+					subHeader {
+						subHeader
+					}
+				}
+			}
+		}
+	}
+`;
+
+export default PageTemplate;
