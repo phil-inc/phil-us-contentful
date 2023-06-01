4aceed57f5011d2a15f87f9f2abffdd0c68e6fb7
sailesh <saileshkarki@lftechnology.com>
2023-06-01 11:15:53 +0545

    Refactor code for code snippet component - include support for dynamic identifiers - add trustpilot reload on component mount only on identifier match

--- src/components/common/CodeSnippet/ParseContent.tsx
+++ src/components/common/CodeSnippet/ParseContent.tsx
@@ -1,20 +1,64 @@
-import React from 'react';
-import parse from 'html-react-parser';
+import React, {useEffect, type FC, type ReactNode, ReactElement} from 'react';
+import parse, {type Element, type HTMLReactParserOptions, attributesToProps} from 'html-react-parser';
 import {sanitize} from 'isomorphic-dompurify';
+import {PHIL_TRUSTPILOT_IDENTIFIER} from 'constants/identifiers';
 
-type ParseContentProps = {content?: string | undefined};
+type ParseContentProps = {
+	content?: string;
+};
 
-export const ParseContent: React.FC<ParseContentProps> = ({content}) => {
-	React.useEffect(() => {
-		const ref = document.getElementById('phil-trustpilot-identifier');
-		// If window.Trustpilot is available it means that we need to load the TrustBox from our ref.
-		// If it's not, it means the script you pasted into <head /> isn't loaded  just yet.
-		// When it is, it will automatically load the TrustBox.
-		if (ref && window.Trustpilot) {
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-			window.Trustpilot.loadFromElement(ref, true);
+type ElementWithId = {
+	id?: string;
+};
+
+// Function to safely retrieve ID from a parsed JSX element or an array of elements
+const extractIdFromElement = (element: ReactNode): string | undefined => {
+	if (typeof element === 'string') {
+		return;
+	}
+
+	if (React.isValidElement(element)) {
+		const {id} = element.props as ElementWithId;
+		return id ?? undefined;
+	}
+
+	if (Array.isArray(element)) {
+		const validElements = element.filter(React.isValidElement);
+		for (const child of validElements) {
+			const id = extractIdFromElement(child);
+			if (id) {
+				return id;
+			}
 		}
-	}, []);
+	}
 
-	return <div>{content && parse(sanitize(content))}</div>;
+	return undefined;
+};
+
+export const ParseContent: FC<ParseContentProps> = ({content}) => {
+	const parsingOptions: HTMLReactParserOptions = {
+		replace(domNode: Element) {
+			if (domNode.attribs) {
+				const props = attributesToProps(domNode.attribs);
+				return <div {...props} />;
+			}
+		},
+	};
+
+	const sanitizedContent = sanitize(content ?? '');
+	const parsedContent = parse(sanitizedContent, parsingOptions);
+
+	useEffect(() => {
+		const elementId = extractIdFromElement(parsedContent);
+
+		if (elementId) {
+			const element = document.getElementById(elementId);
+
+			if (elementId === PHIL_TRUSTPILOT_IDENTIFIER && element && window?.Trustpilot) {
+				window.Trustpilot?.loadFromElement(element, true);
+			}
+		}
+	}, [parsedContent]);
+
+	return <div>{parsedContent}</div>;
 };
