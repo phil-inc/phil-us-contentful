c820135020452bf88095b8e1fd4cbab98e110555
Sailesh <saileshkarki@lftechnology.com>
2023-06-03 15:54:00 +0545

    update career api to use ashby api

--- netlify/edge-functions/getCareerListing.ts
+++ netlify/edge-functions/getCareerListing.ts
@@ -1,15 +1,9 @@
-import {parse} from 'https://deno.land/x/xml@2.1.1/mod.ts';
 import type {Config} from 'https://edge.netlify.com';
 
 const getCareerListing = async (request: Request) => {
-	let origin;
-
-	// fetch allowed origins from netlify
-	const allowedOrigins = Deno.env.get('ALLOWED_ORIGINS');
 	const uri = Deno.env.get('ISOLVEDHIRE_FEED_URI');
-
-	if (allowedOrigins == undefined || uri == undefined) {
-		const error = "Failed to initiate function."
+	if (!uri) {
+		const error = 'Failed to initiate function. URI is undefined.';
 		console.error(error);
 		return new Response(JSON.stringify({error}), {
 			status: 500,
@@ -19,51 +13,47 @@ const getCareerListing = async (request: Request) => {
 		});
 	}
 
+	let origin: string;
 	try {
-		origin = request.headers.get('origin') || new URL(request.headers.get('referer') || '').origin;
+		origin = request.headers.get('origin') || new URL(request.headers.get('referer') || '').origin || '';
 	} catch (err) {
-		// This will be triggered if the URL constructor fails, e.g., due to an invalid URL in the "referer" header.
-		console.log('Unable to determine request origin from headers', err);
-		// Default origin if both headers are not available or invalid.
+		console.error('Unable to determine request origin from headers', err);
 		origin = '';
 	}
 
-	// If the origin is not allowed or not the same origin, deny the request
+	const allowedOrigins = Deno.env.get('ALLOWED_ORIGINS')?.split(',') || [];
 	if (!allowedOrigins.includes(origin)) {
 		return new Response(JSON.stringify({error: 'Not allowed'}), {
 			status: 403,
 			headers: {
 				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': origin,
 			},
 		});
 	}
 
 	try {
 		const response = await fetch(uri);
-
-		const text = await response.text();
-		const json = parse(text);
-
 		if (!response.ok) {
-			throw Error('Fetch career feed failed!');
+			throw new Error('Fetch career feed failed!');
 		}
 
-		const jobs = json?.source?.job
-			? json?.source?.job?.map((j: any) => {
-					const locationString = [j.city, j.state, j.country];
-					const ls = locationString.filter((item: string) => Boolean(item)).join(', ');
+		const {jobs} = await response.json();
+		const myJobs = jobs?.map(
+			({
+				title,
+				department,
+				jobUrl,
+				address: {
+					postalAddress: {addressLocality, addressRegion, addressCountry},
+				},
+			}: any) => {
+				const location = [addressLocality, addressRegion, addressCountry].filter(Boolean).join(', ');
+				return {title, location, department, url: jobUrl};
+			}
+		);
 
-					return {
-						title: j.title,
-						url: j.url,
-						location: ls,
-						department: j.department,
-					};
-			  })
-			: [];
-
-		// Return JSON response
-		return new Response(JSON.stringify(jobs), {
+		return new Response(JSON.stringify(myJobs), {
 			status: 200,
 			headers: {
 				'cache-control': 'public, s-maxage=120',
@@ -73,7 +63,7 @@ const getCareerListing = async (request: Request) => {
 		});
 	} catch (error) {
 		console.error(error);
-		return new Response(JSON.stringify({error: `Failed to parse XML: ${error.message}`}), {
+		return new Response(JSON.stringify({error: `Failed to fetch or parse jobs: ${error.message}`}), {
 			status: 500,
 			headers: {
 				'Content-Type': 'application/json',
