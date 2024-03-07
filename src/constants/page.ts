e3f27dc76fd52c8ecf00ffa5c85eb32c8e31cd77
Sailesh <saileshkarki@lftechnology.com>
2024-03-07 11:31:32 +0545

    update healthcare providers const to providers

--- src/constants/page.ts
+++ src/constants/page.ts
@@ -1,5 +1,5 @@
 export const CONTACT_PAGE = 'Contact';
-export const HCP_PAGE = 'Healthcare Providers';
+export const HCP_PAGE = 'Providers';
 export const HOME = 'Home';
 export const RESOURCES = 'Resources';
 export const PATIENTS_PAGE = 'Patients';
