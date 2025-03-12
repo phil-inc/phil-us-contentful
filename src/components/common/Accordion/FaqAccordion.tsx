320e4503c7ff44b00db9536f0b1d16ac5b5473b9
swornima balay shrestha <swornimashrestha@lftechnology.com>
2025-03-12 22:39:46 +0545

    MRTG-1121: Fix UI in FAQ

--- src/components/common/Accordion/FaqAccordion.tsx
+++ src/components/common/Accordion/FaqAccordion.tsx
@@ -59,9 +59,9 @@ export const FaqAccordion = ({ resource }: any) => {
         transitionDuration={0}
         chevron={
           isOpen ? (
-            <IconMinus size={24} style={{ color: "#000" }} />
+            <IconMinus size={28} style={{ color: "#000" }} />
           ) : (
-            <IconPlus size={24} style={{ color: "#000" }} />
+            <IconPlus size={28} style={{ color: "#000" }} />
           )
         }
         styles={{
@@ -83,6 +83,10 @@ export const FaqAccordion = ({ resource }: any) => {
             color: "#525252",
             margin: "0px 0px 20px 0px",
           },
+          chevron: {
+            width: "28px",
+            height: "28px"
+          }
         }}
       >
         <Accordion.Item
@@ -103,12 +107,15 @@ export const FaqAccordion = ({ resource }: any) => {
     return (
       <Accordion
         transitionDuration={0}
+        chevronSize={28}
+        classNames={{
+          chevron: classes.chevron
+        }}
         styles={{
           content: {
             background: "#f4f4f4",
             marginBottom: "20px",
             padding: "28px",
-            fontSize: "20px",
             color: "#525252",
           },
           control: {
@@ -134,6 +141,10 @@ export const FaqAccordion = ({ resource }: any) => {
   return (
     <Accordion
       transitionDuration={0}
+      chevronSize={28}
+        classNames={{
+          chevron: classes.chevron
+        }}
       styles={{
         content: {
           marginBottom: "20px",
