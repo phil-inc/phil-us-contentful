a823134ffdf519f580f537a2edeb88cfe8fd39ca
tamakhusunder <tamakhusunder@gmail.com>
2025-05-29 19:12:21 +0545

    fix: fix the card in the home page with new design

--- src/constants/identifiers.ts
+++ src/constants/identifiers.ts
@@ -4,3 +4,5 @@ export const CENTER_LIFE_SCIENCES_CARD_TAG =
   "CENTER_COMPONENT_CHILDREN_LIFE_SCIENCES_CARD";
 
 export const THANKS_FOR_YOUR_INTEREST = "Thanks for your interest in PhilRx"
+
+export const MEDICATION_ACCESS_SIMPLIFIED = "Medication Access, Simplified"
