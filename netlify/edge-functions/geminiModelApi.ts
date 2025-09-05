477e4b9b0076ab89f00d23cc2fabeeaed69ee5e0
tamakhusunder <tamakhusunder@gmail.com>
2025-09-05 17:19:27 +0545

    fix: add the logs for testing

--- netlify/edge-functions/geminiModelApi.ts
+++ netlify/edge-functions/geminiModelApi.ts
@@ -12,6 +12,7 @@ const geminiHandler = async (request: Request) => {
   }
   const GEMINI_MODEL = "gemini-2.5-flash";
   const GEMINI_API_KEY = Deno.env.get("GEMINI_API_KEY");
+  console.log("Entering geminiHandler")
 
   if (!GEMINI_API_KEY) {
     const error = "Failed to initiate function. GEMINI_API_KEY is undefined.";
@@ -24,44 +25,46 @@ const geminiHandler = async (request: Request) => {
     });
   }
 
-  // ----------commented our for testing
-  // let origin: string;
-  // try {
-  //   origin =
-  //     request.headers.get("origin") ||
-  //     new URL(request.headers.get("referer") || "").origin ||
-  //     "";
-  // } catch (err) {
-  //   console.error("Unable to determine request origin from headers", err);
-  //   origin = "";
-  // }
+  let origin: string;
+  try {
+    origin =
+      request.headers.get("origin") ||
+      new URL(request.headers.get("referer") || "").origin ||
+      "";
+  } catch (err) {
+    console.error("Unable to determine request origin from headers", err);
+    origin = "";
+  }
+  console.log("Request origin:", origin);
 
-  // const allowedOrigins = Deno.env.get("ALLOWED_ORIGINS")?.split(",") || [];
-  // if (!allowedOrigins.includes(origin)) {
-  //   return new Response(JSON.stringify({ error: "Not allowed" }), {
-  //     status: 403,
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "Access-Control-Allow-Origin": origin,
-  //     },
-  //   });
-  // }
+  const allowedOrigins = Deno.env.get("ALLOWED_ORIGINS")?.split(",") || [];
+  if (!allowedOrigins.includes(origin)) {
+    return new Response(JSON.stringify({ error: "Not allowed" }), {
+      status: 403,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": origin,
+      },
+    });
+  }
 
   const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/${GEMINI_MODEL}-preview-05-20:generateContent?key=${GEMINI_API_KEY}`;
-  const body = JSON.parse(request.body);
-  const payload = {
-    contents: [{ parts: [{ text: body.question }] }],
-    systemInstruction: {
-      parts: [{ text: TRAINNING_DATA }],
-    },
-  };
-
+  console.log("Payload",request)
   try {
+    const body = JSON.parse(request.body);
+    const payload = {
+      contents: [{ parts: [{ text: body.question }] }],
+      systemInstruction: {
+        parts: [{ text: TRAINNING_DATA }],
+      },
+    };
     const response = await fetch(GEMINI_API_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload),
     });
+      console.log("response:", response);
+
 
     if (!response.ok) {
       return new Response(JSON.stringify({ error }), {
@@ -73,6 +76,7 @@ const geminiHandler = async (request: Request) => {
     }
 
     const data = await response.json();
+    console.log("Gemini API response data:", data);
 
     return new Response(JSON.stringify(data), {
       status: 200,
