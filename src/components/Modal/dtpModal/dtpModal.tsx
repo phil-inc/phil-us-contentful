06d2b8dfcf093aff3834c8a73c363f213f6361cf
tamakhusunder <tamakhusunder@gmail.com>
2025-09-19 14:17:28 +0545

    fix:width of modal in large screen

--- src/components/Modal/dtpModal/dtpModal.tsx
+++ src/components/Modal/dtpModal/dtpModal.tsx
@@ -1,15 +1,14 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import { Modal } from "@mantine/core";
-import { SHOW_DTP_MODAL } from "constants/global.constant";
-
-import * as classes from "./dtpModal.module.css";
-
-import { ContentfulModal } from "types/modal";
-import { useIsLaptop } from "hooks/useIsLaptop";
+import { SCREEN_SIZES, SHOW_DTP_MODAL } from "constants/global.constant";
+import { useViewportSize } from "@mantine/hooks";
 
 import LaunchDtp from "components/LaunchDtp/LaunchDtp";
 
 import PageContext from "contexts/PageContext";
+
+import { ContentfulModal } from "types/modal";
+import { HOME } from "constants/page";
 
 type DTPModalProps = {
   contentfulModalNodes: ContentfulModal[];
@@ -17,9 +16,21 @@ type DTPModalProps = {
 
 const DTPModal: React.FC<DTPModalProps> = ({ contentfulModalNodes }) => {
   const context = useContext(PageContext);
-  const isHomePage = context.title === "Home";
-  const isLaptopScreen = useIsLaptop();
+  const isHomePage = context.title === HOME;
   const [opened, setOpened] = useState(false);
+
+  const { width } = useViewportSize();
+  const modalSize = useMemo(() => {
+    if (width <= SCREEN_SIZES.MOBILE) {
+      return "auto";
+    } else if (width <= SCREEN_SIZES.DESKTOP) {
+      return "80%";
+    } else if (width <= SCREEN_SIZES.LARGE_DESKTOP) {
+      return "70%";
+    } else {
+      return "auto";
+    }
+  }, [width]);
 
   const modalData = contentfulModalNodes[0];
 
@@ -43,12 +54,11 @@ const DTPModal: React.FC<DTPModalProps> = ({ contentfulModalNodes }) => {
   return (
     <>
       <Modal
-        className={classes.modal}
         opened={opened}
         onClose={handleClose}
         withCloseButton={false} // hides close button
         centered
-        size={isLaptopScreen ? "80%" : "auto"}
+        size={modalSize}
         padding={0}
         radius={"md"}
         overlayProps={{
