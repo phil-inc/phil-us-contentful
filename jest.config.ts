bdb4185ed4ab78d9c55e8f12ba5dddc112762938
tamakhusunder <tamakhusunder@gmail.com>
2025-10-21 18:22:37 +0545

    test: add the unit test of date and trustpilot

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@jest/types";
 const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
-  roots: ["./src", "./test"],
+  roots: ["<rootDir>/src"],
   transform: {
     "^.+\\.(t|j)sx?$": "ts-jest",  // let ts-jest handle TypeScript + ESM
   },
