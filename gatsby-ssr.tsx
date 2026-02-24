4fb4007aa58aea646421dc15baf3cb69a0d678cf
Sanjeev Rai <sunzeevraee@gmail.com>
2026-02-24 17:52:25 +0545

    feat: Add CookieYes script to head components in gatsby-ssr.tsx

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -4,7 +4,17 @@ import {theme} from './src/layouts/Layout/theme';
 
 export const onPreRenderHTML = ({getHeadComponents, replaceHeadComponents}) => {
 	const headComponents = getHeadComponents();
-	replaceHeadComponents([...headComponents, <ColorSchemeScript key="color-scheme-script" />]);
+	// Add CookieYes script at the beginning of head
+	const cookieYesScript = (
+		<script
+			key="cookieyes"
+			id="cookieyes"
+			type="text/javascript"
+			src="https://cdn-cookieyes.com/client_data/2dc47b0a0466a26a3289cf91512a2365/script.js"
+		/>
+	);
+	
+	replaceHeadComponents([cookieYesScript, ...headComponents, <ColorSchemeScript key="color-scheme-script" />]);
 };
 
 export const wrapPageElement = ({element}) => {
