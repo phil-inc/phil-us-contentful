2b7dc3a7509322c26710d686d2223d03c548a7ab
Sanjeev Rai <sunzeevraee@gmail.com>
2026-03-01 17:15:44 +0545

    refactor: update Google Tag Manager ID for improved tracking

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -43,7 +43,7 @@ const gtmScript = (
 				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-				})(window,document,'script','dataLayer','GTM-P8P2538M');
+				})(window,document,'script','dataLayer','GTM-MF95PNSX');
 			`,
 		}}
 	/>
