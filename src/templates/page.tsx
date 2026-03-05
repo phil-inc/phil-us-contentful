62b061586f0d56caad7325212d34110e3400d558
Sanjeev Rai <sunzeevraee@gmail.com>
2026-03-05 09:39:28 +0545

    feat: add new GraphQL fields for background asset image and header description in page template

--- src/templates/page.tsx
+++ src/templates/page.tsx
@@ -183,6 +183,21 @@ export const query = graphql`
               url
             }
           }
+          backgroundAssetImage3 {
+            gatsbyImageData(
+              resizingBehavior: SCALE
+              placeholder: BLURRED
+              layout: CONSTRAINED
+            )
+            title
+            file {
+              contentType
+              details {
+                size
+              }
+              url
+            }
+          }
           sectionGroupReference {
             ... on ContentfulSection {
               id
@@ -401,6 +416,9 @@ export const query = graphql`
                     id
                   }
                 }
+              }
+              headerDescription {
+                headerDescription
               }
               subHeader {
                 subHeader
@@ -897,6 +915,9 @@ export const query = graphql`
                 id
               }
             }
+          }
+          headerDescription {
+            headerDescription
           }
           subHeader {
             subHeader
