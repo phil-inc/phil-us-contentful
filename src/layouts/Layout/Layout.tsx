9fb1c4088bf4c9eee5cbffa6ad7dea84d042775f
Sanjeev Rai <sunzeevraee@gmail.com>
2026-05-15 18:41:24 +0545

    Fix: Reverted back the CInfo notification to show in the /patients and /faqs page.
    Also, made the width of container equal for all pages and section.

--- src/layouts/Layout/Layout.tsx
+++ src/layouts/Layout/Layout.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { AppShell } from "@mantine/core";
 import { HubspotProvider } from "@aaronhayes/react-use-hubspot-form";
+import { useLocation } from "@reach/router";
 import CFooter from "./CFooter/CFooter";
 import LinkedinInsights from "analytics/LinkedinInsights";
-
+import CInfoBar from "layouts/Layout/CInfoBar/CInfoBar";
 // Import css overrides here
 import "assets/css/index.css";
 
@@ -28,12 +29,16 @@ export function Layout({
   headerTargetBlank = false,
   canHideHeader = false,
 }: LayoutProps) {
+  const location = useLocation();
+  const currentLocationSlug = location.pathname.replace(/^\/|\/$/g, "");
+
   return (
     <>
       <HubspotProvider>
         <AppShell>
           {isProduction && <ZoominfoAnalytics />}
           {!canHideHeader && <MegaNav minimal={minimal} headerTargetBlank={headerTargetBlank} />}
+          <CInfoBar currentLocationSlug={currentLocationSlug}/>
           <>{children}</>
           <CFooter minimal={minimal} />
         </AppShell>
