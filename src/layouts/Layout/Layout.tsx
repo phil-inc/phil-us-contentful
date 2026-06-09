3a63a3868a0f26221cda42c75b5e5567ef479a35
Sanjeev Rai <sunzeevraee@gmail.com>
2026-06-09 18:02:42 +0545

    chore: remove LinkedIn Insights and ZoomInfo analytics integrations
    - Remove LinkedinInsights component and associated analytics script
    - Remove ZoominfoAnalytics component and conditional rendering logic
    - Delete DNS prefetch hints for snap.licdn.com and js.zi-scripts.com in gatsby-ssr.tsx
    - Remove isProduction condition and related imports from Layout component
    - Simplify Layout by removing third-party analytics integrations

--- src/layouts/Layout/Layout.tsx
+++ src/layouts/Layout/Layout.tsx
@@ -3,18 +3,11 @@ import { AppShell } from "@mantine/core";
 import { HubspotProvider } from "@aaronhayes/react-use-hubspot-form";
 import { useLocation } from "@reach/router";
 import StaticFooter from "./MegaFooter/megaFooter";
-import LinkedinInsights from "analytics/LinkedinInsights";
 import CInfoBar from "layouts/Layout/CInfoBar/CInfoBar";
 // Import css overrides here
 import "assets/css/index.css";
 
-import ZoominfoAnalytics from "analytics/ZoominfoAnalytics";
-
 import MegaNav from "layouts/Layout/MegaNav/MegaNav";
-
-const isProduction = process.env.NODE_ENV === "production";
-
-export const Head: React.FC = () => <>{isProduction && <LinkedinInsights />}</>;
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -36,7 +29,6 @@ export function Layout({
     <>
       <HubspotProvider>
         <AppShell>
-          {isProduction && <ZoominfoAnalytics />}
           {!canHideHeader && <MegaNav minimal={minimal} headerTargetBlank={headerTargetBlank} />}
           <CInfoBar currentLocationSlug={currentLocationSlug}/>
           <>{children}</>
